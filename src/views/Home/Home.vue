1930b403d9a1060a2fcbde58c20782431063719e
andre <defauult@default.com>
2018-08-29 10:49:17 -0400

    memu fixed

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -4,7 +4,7 @@
     <Section2/>
     <Section3/>
     <Section6/>
-    <Section9/>
+
     <Footer/>
   </div>
 </template>
@@ -14,10 +14,7 @@ import Footer from '@/components/Footer'
 import Section1 from './Sections/Section-1.vue'
 import Section2 from './Sections/Section-2.vue'
 import Section3 from './Sections/Section-3.vue'
-import Section4 from './Sections/Section-4.vue'
-import Section5 from './Sections/Section-5.vue'
 import Section6 from './Sections/Section-6.vue'
-import Section9 from './Sections/Section-9.vue'
 
 export default {
   name: 'Home',
@@ -26,10 +23,7 @@ export default {
     Section1,
     Section2,
     Section3,
-    Section4,
-    Section5,
     Section6,
-    Section9
   }
 }
 </script>
